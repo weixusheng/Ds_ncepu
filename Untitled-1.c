9aa51a68a41a53dc3b0b459452dc2c3bed8b2712
weixusheng <tronwei@163.com>
2020-03-23 17:05:03 +0800

    单链表

--- Untitled-1.c
+++ Untitled-1.c
@@ -73,14 +73,126 @@ int deleteSqList(SqList *l, int i, STD *x){
 }
 //更新操作
 int updateSqList(SqList l, int i, STD x){
-    if(l.length==0){    
-        printf("线性表没有数据")；return 0;
-    }    
+    if(l.length == 0){
+        printf("线性表没有数据")；return 0；
+    }
+    if(i<=0 || i>l.length){
+        printf("更新位置有问题");return 0;
+    }
+    l.data[i-1] = x;
+    return 1;
 }
-typedef hello{
-    h
+//获取操作
+int getSqList(SqList l, int i, STD *x){
+    if(l.length == 0){
+        printf("线性表没有数据")；return 0；
+    }
+    if(i<=0 || i>l.length){
+        printf("位置有问题")；return 0；
+    }
+    *x = l.data[i-1];
+    return 1;
+}
+//定位操作
+int locationSqList(SqList l, char *x){
+    int i;
+    if(l.length == 0){
+        printf("没有数据")；return 0；
+    }
+    for(i = 0; i<l,length, i++){
+        if(strcmp(l.data[i]).name, x){
+            return i+1
+        }
+    }
+    return 0;
+}
+//遍历操作
+int dispSqList(SqList l){
+    int i;
+    if(l.length == 0){
+        printf("没有数据")；return 0；
+    }
+    for(i=0; i<l.length; i++){
+        printf("%10s &7.2f",l.data[i].name,l.data[i].score);
+    }
+    return 1;
 }
 
+//链式存储
+typedef struct node{
+    ElemType data;
+    struct node *next;
+}LNode_basic, *LinkList_basic;
 
+typedef struct Lnode{
+    STD data;
+    struct Lnode *next;
+}LNode, *LinkList;
+
+//初始化单链表
+int initLinkList(LinkList *l){
+    *l = (LinkList)malloc(sizeof(LinkList));
+    if(*l == 0)return 0;
+    (*l->next) = NULL;
+    return 1;
+}
+
+//插入操作
+int insertLinkList(LinkList l, int i, STD x){
+    LinkList p =l,s;
+    int pos = 0;
+    while(p!=NULL && pos<i-1){
+        p = p->next;
+        pos++;
+    }
+    if(p == null || pos > i-1){
+        printf("插入位置有问题")；return 0；
+    }
+    s = (LinkList)malloc(sizeof(LNode));
+    s->data = x;
+    s->next = p->next;
+    p->next = s;
+    return 1;
+}
+
+//删除操作
+deleteLinkList(LinkList l, int i, STD *x){
+    LinkList p = l,q;
+    int pos = 0;
+    while(p->next != NULL && pos < i-1){
+        p = p->next;
+        pos++;
+    }
+    if(p->next == NULL || i-1 < pos){
+        printf("删除位置有问题")；return 0；
+    }
+    q = p->next;
+    p->next = q->next;
+    x = q->data;
+    free(q);
+    return 1;
+}
+
+//更新操作
+int updateLinkList(LinkList l, int i, STD x){
+    int pos = 0;
+    LinkList p = l;
+    while(p->next != NULL && pos < i){
+        p = p->next;
+        pos++;
+    }
+    if(p->next == NULL || i > pos){
+        printf("更新位置有问题")；return 0；
+    }
+    p->data = x;
+    return 1;
+}
+
+//获取操作
+int getLinkList(LinkList l, int i, STD *x){
+    LinkList p = l;
+    int pos = 0;
+    while(p->next != NULL && pos < i-1)
+}
 
 
