cc67c15dff7ace1af1d1a6940169b9b6c0d34821
weixusheng <tronwei@163.com>
2020-04-16 21:04:16 +0800

    最小生成树

--- DS的基础操作.c
+++ DS的基础操作.c
@@ -1921,8 +1921,8 @@ void DFS_h(MGraph g, int i, int path[], int visited[], int *n){
 
 //判断图中是否存在环-基于深度优先
 void DFSloop(MGraph g, int i, int visited[]){
-    visited[i] = 1;
-    for(int di=0, vi=0, j=0; j<g.vexNum; j++){
+    visited[i] = 1;         //当前结点访问
+    for(int di=0, vi=0, j=0; j<g.vexNum; j++){          //判断边是否都被访问
         if(g.arcs[i][j]){
             di++;
             if(visited[j]){
@@ -1930,11 +1930,11 @@ void DFSloop(MGraph g, int i, int visited[]){
             }
         }
     }
-    if(di == vi && di!= 1){
+    if(di == vi && di!= 1){         //判断是否存在环
         printf("有回路");
         return;
     }
-    for(j=0; j<g.arcNum; j++){
+    for(j=0; j<g.arcNum; j++){          //dfs遍历,递归判断
         if(g.arcs[i][j] && visited[j] == 0){
             DFSloop(g, j, visited);
         }
@@ -1943,33 +1943,33 @@ void DFSloop(MGraph g, int i, int visited[]){
 
 //求无向图的顶点a到i之间的简单路径
 void DFSSearchPath(MGraph g, int v, int s, char path[], int visited[], int *found){
-    visited[v-1] = 1;
-    append(path, g.vertex[v-1]);
-    for(j = 0; j<g.vexNum && (*found); j++){
-        if(g.arcs[v-1][j] == 1){
-            if(s == j+1){
+    visited[v-1] = 1;           //访问该结点
+    append(path, g.vertex[v-1]);            //结点进数组path
+    for(j = 0; j<g.vexNum && !(*found); j++){
+        if(g.arcs[v-1][j] == 1){            //边存在
+            if(s == j+1){           //当前结点为为目标节点,则找到路径
                 *found = 1;
                 append(path, g.vertex[j]);
             }
-            else if(visited[j] == 0){
+            else if(visited[j] == 0){           //dfs递归遍历
                 DFSSearchPath(g, j+1, s, path, visited, *found);
             }
         }
     }
-    if(!(*found)){
+    if(!(*found)){          //递归子结点都访问完,删除当前结点
         delete(path);
     }
 }
 
 //求无向图中vi和vj之间的最短路径
 int BFSsearchPath(MGraph g, int vi, int vj, SqQueue *q){
-    for(int i=0; i<g.vexNum, i++){
+    for(int i=0; i<g.vexNum, i++){          //初始化 visted[]
         visited[i] = 0;
     }
     initQueue(q);
-    EnQueue_bfs(q, vi);
-    visited[vi] = 1;
-    while(!QueueEmpty(q)){
+    EnQueue_bfs(q, vi);         //第一个节点访问
+    visited[vi] = 1; 
+    while(!QueueEmpty(q)){          //广度优先遍历
         DeQueue_bfs(q, &v);
         for(w = 0; w<g.vexNum; w++){
             if(g.arcs[v][w] && visited[w]){
@@ -1997,7 +1997,7 @@ typedef struct {
 }bfsqueue;
 
 
-void EnQueue_bfs(bfsqueue *q, Elemdata data){
+void EnQueue_bfs(bfsqueue *q, Elemdata data){           //prior指向队头front
     p = (bfsqueue)malloc(sizeof(bfsnode));
     p->data = data;
     p->next = NULL;
@@ -2009,6 +2009,91 @@ void EnQueue_bfs(bfsqueue *q, Elemdata data){
 void Dequeue_bfs(bfsqueue *q, Elemdata *data){
     *data = q->front;
     q->front = q->front->next;
+}
+
+//最小生成树-prim
+#define maxcost 10000
+typedef struct{
+    int vi;
+    int weight;
+}Lowcost;
+
+void prim(MGraph g){
+    int mincost, i, j, k;
+    Lowcost *lowcost = (Lowcost*)malloc(sizeof(Lowcost) * g.vexNum);            //申请 lowcost 数组空间
+    for(i=0; i<g.vexNum; i++){          //初始化 lowcost 数组
+        lowcost[i].vi = 0;
+        lowcost[i].weight = g.arcs[0][i];
+    }
+    lowcost[0].weight = 0;          //第一个节点进入生成树中,weight=0
+    for(i=0; i<g.vexNum; i++){          //找到当前离树权值最小的结点
+        mincost = maxcost;
+        for(j=0; j<g.vexNum; j++){
+            if(lowcost[j].weight < mincost && lowcost[j].weight != 0){
+                mincost = lowcost[j].weight;
+                k = j;
+            }
+        }
+        lowcost[k].weight = 0;          //权值最小结点进生成树,weight=0
+        for(j=0; j<g.vexNum; g++){          //更新其余未进入生成树结点的权值
+            if(g.ars[k][j] < lowcost[j].weight){
+                lowcost[j] = g.ars[k][j];
+                lowcost[j].vi = k;
+            }
+        }
+    }
+}
+
+//最小生成树-kruskal
+typedef char afc[MAX];
+void kruskal(MGraph g){
+    int i, j, k, t., p, q, x[MAX][4];
+    zfc c[MAX];
+    for(i=0; i<g.vexNum; i++){
+        c[i][0] = i+48;         //初始化,下标转字符串
+        c[i][1] = 0;            //字符串结束标志
+    }
+    for(i=0; i<g,vexNum; i++){          //初始化所有边,放进 x[max][4] 数组
+        for(j=i; j<g.vexNum; j++){
+            if(g.arcs[i][j] != 0 && g.arcs[i][j] != maxcost){
+                x[k][0] = i;
+                x[k][1] = j;
+                x[k][2] = g.arcs[i][j];
+                x[k][3] = 0
+                k++;
+            }
+        }
+    }
+    for(i=0; i<g.arcNum; i++){          //对所有边进行排列,权值从小到大
+        for(j=0; j<g.arcNum-1-i; j++){
+            if(x[j][2] > x[j+1][2]){
+                for(p=0; p<4; p++){
+                    t = x[j][p];
+                    x[j][p] = x[j+1][p];
+                    x[j+1][p] = t;
+                }
+            }
+        }
+    }
+    for(i=0, k=0; i<g.arcNum; i++){         //对所有边进行从小到大判断
+        p = find(c, g.vexNum, x[i][0]+48);
+        q = find(c, g.vexNum, x[i][1]+48);          //判断边的两个结点是否成环
+        if(p != q){
+            stract(c[p], c[q]);
+            c[q][0] = 0;
+            k++;
+            x[i][3] = 1;
+        }
+        if(k == g.vexNum-1){            //当有 vexnum-1 条边时,最小生成树构造完成
+            break;
+        }
+    }
+    printf("%7c%7c%6s", "顶点", "顶点", "权值");
+    for(i=0; i<g.arcNum; i++){          //输出边
+        if(x[i][3] == 1){
+            printf("%7c%7c%6d", g.vertex[x[i][0]], g.vertex[x[i][1]], x[i][2])
+        }
+    }
 }
 
 #pragma endregion
